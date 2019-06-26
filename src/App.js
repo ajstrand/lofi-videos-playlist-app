e9643880e69c70c2510e470dbd863d9bc7540609
Alex <ajstrand8@gmail.com>
2019-06-25 22:42:29 -0400

    add test request on the front end

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ function App() {
 
   useEffect(() => {
     if (videoList.length === 0) {
+      axios.get('/api/test').then(response => console.log(response.data))
       const graphQL_URL = '/api/graphql';
       axios
         .post(graphQL_URL, { query: GET_DATA })
