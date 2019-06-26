1583fd9b0cb9ba2a7160bb1afd925894181b7116
Alex <ajstrand8@gmail.com>
2019-06-25 23:04:21 -0400

    trying out a stackoverflow answer

--- server.js
+++ server.js
@@ -108,7 +108,14 @@ const schema = new GraphQLSchema({
 
 app.use(helmet());
 
-app.use(express.static(path.join(__dirname, '../build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.get('/api/test', (req, res) => {
   const hello = "hello from the server, this is a test";
